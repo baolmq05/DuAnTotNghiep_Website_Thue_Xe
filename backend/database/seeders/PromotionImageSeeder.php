a36892134871e624fe1955acdd852de6b9c04b82
quoc toan <nguyenthimysuong99999@gmail.com>
2026-07-04 16:22:16 +0700

    [FIX] Change PromotionImage Items in Seeder

--- backend/database/seeders/PromotionImageSeeder.php
+++ backend/database/seeders/PromotionImageSeeder.php
@@ -15,19 +15,19 @@ class PromotionImageSeeder extends Seeder
     {
         PromotionImage::create([
             'promotion_id' => 1,
-            'image_url' => 'https://res.cloudinary.com/djbobb5oe/image/upload/v1782706265/e066d0fb-d713-4aac-a05d-c0c022530795_vk9zcj.jpg'
+            'image_url' => 'https://res.cloudinary.com/dfmoftnpw/image/upload/v1783153605/z8006884307236_135cff544e4f013d18015dc69071ba52_bzswc3.jpg'
         ]);
         PromotionImage::create([
             'promotion_id' => 2,
-            'image_url' => 'https://res.cloudinary.com/djbobb5oe/image/upload/v1782706265/e066d0fb-d713-4aac-a05d-c0c022530795_vk9zcj.jpg'
+            'image_url' => 'https://res.cloudinary.com/dfmoftnpw/image/upload/v1783153605/z8006884269175_9b27d8cc121d2d60a2208ef64f41ec93_twggmx.jpg'
         ]);
         PromotionImage::create([
             'promotion_id' => 3,
-            'image_url' => 'https://res.cloudinary.com/djbobb5oe/image/upload/v1782706265/e066d0fb-d713-4aac-a05d-c0c022530795_vk9zcj.jpg'
+            'image_url' => 'https://res.cloudinary.com/dfmoftnpw/image/upload/v1783153605/z8006884925560_9d06b15e0252024ce72013e5f13adf06_zpxfge.jpg'
         ]);
         PromotionImage::create([
             'promotion_id' => 4,
-            'image_url' => 'https://res.cloudinary.com/djbobb5oe/image/upload/v1782706265/e066d0fb-d713-4aac-a05d-c0c022530795_vk9zcj.jpg'
+            'image_url' => 'https://res.cloudinary.com/dfmoftnpw/image/upload/v1783153605/z8006887853933_6d3eed6000f370adcab1c18c0863d4fe_xfpd43.jpg'
         ]);
     }
 }
