c45a8961faefe0e120275a0e5769c8f31714b664
hoangphi0208 <vohoangphi75@gmail.com>
2026-07-10 14:03:25 +0700

    [FIX] Generate notifications when approving or rejecting driver's licenses.

--- backend/app/Models/DrivingLicense.php
+++ backend/app/Models/DrivingLicense.php
@@ -8,4 +8,30 @@ class DrivingLicense extends Model
 {
     //
     protected $fillable = ['full_name', 'image', 'driving_license_number', 'DOB', 'status'];
+
+    protected static function booted()
+    {
+        static::updated(function ($drivingLicense) {
+            if ($drivingLicense->wasChanged('status')) {
+                $user = User::where('driving_license_id', $drivingLicense->id)->first();
+                if ($user) {
+                    $status = (int) $drivingLicense->status;
+                    $message = '';
+                    if ($status === 1) {
+                        $message = 'Giấy phép lái xe của bạn đã được duyệt thành công. Giờ đây bạn đã có thể tiến hành thuê xe!';
+                    } elseif ($status === 2) {
+                        $message = 'Giấy phép lái xe của bạn đã bị từ chối do thông tin chưa hợp lệ. Vui lòng cập nhật lại hình ảnh hoặc thông tin GPLX chính xác.';
+                    }
+
+                    if ($message) {
+                        Notification::create([
+                            'user_id' => $user->id,
+                            'message' => $message,
+                            'is_read' => '0',
+                        ]);
+                    }
+                }
+            }
+        });
+    }
 }
